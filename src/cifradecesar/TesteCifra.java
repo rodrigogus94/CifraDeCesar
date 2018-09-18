906e0dedc0beba1612bb08ce27f56686398524ae
Rodri <Rodri@DESKTOP-JEDM4TK.mshome.net>
2018-09-18 09:49:00 -0300

    Login

--- src/cifradecesar/TesteCifra.java
+++ src/cifradecesar/TesteCifra.java
@@ -16,19 +16,43 @@ public class TesteCifra {
 
     public static void main(String[] args) {
         Random chaveRandom = new Random();
+        int chave=0, opc = 1, chaveD = 1;
+        
+        String texto="";
         CifraDeCesar t = new CifraDeCesar();
-        String texto = JOptionPane.showInputDialog("Digite um texto" );
-        
-        
-        //int chave = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para chave da criptografia"));
+
+        while (opc != 0) {
+            opc = Integer.parseInt(JOptionPane.showInputDialog("Digite uma Opção"
+                    + "\n 1 - criptar um texto"
+                    + "\n 2 - descripta o texto"
+                    + "\n 0 - Sair"));
+            switch (opc) {
+                case 1:
+                    texto = JOptionPane.showInputDialog("Digite um texto");
+                    int input = JOptionPane.showConfirmDialog(null, "Cirar chave randomicamente",
+                            "Escolha uma opção", JOptionPane.YES_NO_OPTION);
+                    if (input == 0) {
+                        chave = chaveRandom.nextInt(10000);
+                    } else {
+                        chave = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para chave da criptografia"));
+                    }
+                    JOptionPane.showMessageDialog(null, "Texto Criptografado!");
+                    break;
+                case 2:
+                  t.Foca(chave, chaveD, texto); 
+                  JOptionPane.showMessageDialog(null, "Texto descriptografado:\n" + texto);
+                    break;
+                case 0:
+                    break;
+            }
+        }
+
+        //
         //int chaveD = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para chave da criptografia"));
         
-        int chave = chaveRandom.nextInt(10000);
-        int chaveD = 1;
 
         
-        t.Foca(chave, chaveD, texto);
-       
+
     }
 
 }
